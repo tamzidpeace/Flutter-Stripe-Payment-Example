664a529a1c4f6fef01663e26f4078fd78f1cd24a
tamzidpeace <tamjedpeace@gmail.com>
2021-12-17 08:14:58 +0600

    flutter stripe

---
+++ android/app/src/main/kotlin/com/example/stripetest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stripetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe_payment/flutter_stripe_payment.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? _paymentMethodId;
+  String _errorMessage = "";
+  final _stripePayment = FlutterStripePayment();
+  var _isNativePayAvailable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    //_stripePayment.setStripeSettings("{STRIPE_PUBLISHABLE_KEY}", "{STRIPE_APPLE_PAY_MERCHANTID}");
+    _stripePayment.setStripeSettings('pk_test_zNc5tquatSbXljddf76ksHaF00mTm5lWhM');
+    _stripePayment.onCancel = () {
+      print("the payment form was cancelled");
+    };
+    // checkIfAppleOrGooglePayIsAvailable();
+  }
+
+  void checkIfAppleOrGooglePayIsAvailable() async {
+    var available = await _stripePayment.isNativePayAvailable();
+    setState(() {
+      _isNativePayAvailable = available;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stripe App Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _paymentMethodId != null
+                  ? Text(
+                      "Payment Method Returned is $_paymentMethodId",
+                      textAlign: TextAlign.center,
+                    )
+                  : Container(
+                      child: Text(_errorMessage),
+                    ),
+              ElevatedButton(
+                child: Text("Create a Card Payment Method"),
+                onPressed: () async {
+                  var paymentResponse = await _stripePayment.addPaymentMethod();
+                  setState(() {
+                    if (paymentResponse.status == PaymentResponseStatus.succeeded) {
+                      _paymentMethodId = paymentResponse.paymentMethodId;
+                      log(_paymentMethodId.toString());
+                    } else {
+                      _errorMessage = paymentResponse.errorMessage!;
+                      log(_errorMessage);
+                    }
+                  });
+
+                  
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
